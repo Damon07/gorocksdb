06a30efc168adad82b5f80f0d1b9ca3fc090ffd4
wangmm <wangmm@tuya.com>
2021-05-29 16:06:19 +0800

    add libc
    Signed-off-by: wangmm <wangmm@tuya.com>

--- staticflag_linux.go
+++ staticflag_linux.go
@@ -1,4 +1,4 @@
 package gorocksdb
 
-// #cgo LDFLAGS: -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -l:libdl.a -l:librocksdb.a
+// #cgo LDFLAGS: -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -l:libc.a -l:libdl.a -l:librocksdb.a
 import "C"
